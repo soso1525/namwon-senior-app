7b964873f8acfa3da69ee79b34bc33e40f2d4e38
j.sohyun <j.sohyun@bit.kr>
2026-06-18 15:46:26 +0900

    release v1.0.3(3).

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "kr.go.namwon.seniorcenter.app"
         minSdk = 23
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 3
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
